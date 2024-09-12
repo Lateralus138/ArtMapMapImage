984715fba8c273eff50c3c4b881faf2edd725755
Ian Pride <faithnomoread@yahoo.com>
2024-09-11 19:56:42 -0500

    Build tests - 00:56:39 UTC - Sep 12, 2024

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,9 +1,7 @@
 $SCRIPTDIR = $PSScriptRoot
 $PARENTDIR = (Get-Item $SCRIPTDIR ).parent.FullName
-$WINDOWSDIR = "${PARENTDIR}\windows"
-$BUILDDIR = "$($PARENTDIR)\build\windows"
-$BUILDDIR86 = "$($BUILDDIR)\x86"
-$BUILDDIR64 = "$($BUILDDIR)\x64"
+$SOURCEDIR = "${PARENTDIR}\source"
+$BUILDDIR = "$($PARENTDIR)\build\"
 if (-Not (Test-Path "$($BUILDDIR)" -PathType Container))
 {
   New-Item "$($BUILDDIR)" -ItemType Directory
@@ -12,32 +10,18 @@ if (-Not (Test-Path "$($BUILDDIR)" -PathType Container))
     Exit 255
   }
 }
-if (-Not (Test-Path "$($BUILDDIR86)" -PathType Container))
-{
-  New-Item "$($BUILDDIR86)" -ItemType Directory
-  if (-Not (Test-Path "$($BUILDDIR86)" -PathType Container))
-  {
-    Exit 254
-  }
-}
-if (-Not (Test-Path "$($BUILDDIR64)" -PathType Container))
-{
-  New-Item "$($BUILDDIR64)" -ItemType Directory
-  if (-Not (Test-Path "$($BUILDDIR64)" -PathType Container))
-  {
-    Exit 253
-  }
-}
-Set-Location "$($WINDOWSDIR)"
-MSBuild /p:Configuration=Release /p:Platform=x86
-if (-Not (Test-Path "$($WINDOWSDIR)\Release\am_closest.exe" -PathType Leaf))
-{
-  Exit 252
-}
-Move-Item -Path "$($WINDOWSDIR)\Release\am_closest.exe" "$($BUILDDIR86)\am_closest-x86.exe"
-MSBuild /p:Configuration=Release /p:Platform=x64
-if (-Not (Test-Path "$($WINDOWSDIR)\x64\Release\am_closest.exe" -PathType Leaf))
-{
-  Exit 251
-}
-Move-Item -Path "$($WINDOWSDIR)\x64\Release\am_closest.exe" "$($BUILDDIR64)\am_closest-x64.exe"
+Set-Location "$($SOURCEDIR)"
+& '.\pyenv_nuitka_setup.ps1' | Exit 254
+& '.\nuitka_compile.ps1' | Exit 253
+# MSBuild /p:Configuration=Release /p:Platform=x86
+# if (-Not (Test-Path "$($SOURCEDIR)\Release\am_closest.exe" -PathType Leaf))
+# {
+#   Exit 252
+# }
+# Move-Item -Path "$($SOURCEDIR)\Release\am_closest.exe" "$($BUILDDIR86)\am_closest-x86.exe"
+# MSBuild /p:Configuration=Release /p:Platform=x64
+# if (-Not (Test-Path "$($SOURCEDIR)\x64\Release\am_closest.exe" -PathType Leaf))
+# {
+#   Exit 251
+# }
+# Move-Item -Path "$($SOURCEDIR)\x64\Release\am_closest.exe" "$($BUILDDIR64)\am_closest-x64.exe"
