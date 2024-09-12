b9d5027f6bb67c9985622a9f3d9b15b4b12f0bac
Ian Pride <faithnomoread@yahoo.com>
2024-09-11 20:18:06 -0500

    Build tests - 01:18:05 UTC - Sep 12, 2024

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -13,15 +13,9 @@ if (-Not (Test-Path "$($BUILDDIR)" -PathType Container))
 Set-Location "$($SOURCEDIR)"
 & '.\pyenv_nuitka_setup.ps1' || Exit 254
 & '.\nuitka_compile.ps1' || Exit 253
-# MSBuild /p:Configuration=Release /p:Platform=x86
-# if (-Not (Test-Path "$($SOURCEDIR)\Release\am_closest.exe" -PathType Leaf))
-# {
-#   Exit 252
-# }
-# Move-Item -Path "$($SOURCEDIR)\Release\am_closest.exe" "$($BUILDDIR86)\am_closest-x86.exe"
-# MSBuild /p:Configuration=Release /p:Platform=x64
-# if (-Not (Test-Path "$($SOURCEDIR)\x64\Release\am_closest.exe" -PathType Leaf))
-# {
-#   Exit 251
-# }
-# Move-Item -Path "$($SOURCEDIR)\x64\Release\am_closest.exe" "$($BUILDDIR64)\am_closest-x64.exe"
+if (-Not (Test-Path "$($SOURCEDIR)\mapimage.exe" -PathType Leaf))
+{
+  Exit 254
+}
+Move-Item -Path "$($SOURCEDIR)\mapimage.exe" "$($BUILDDIR)\am_mapimage.exe" || Exit 253
+Exit 0
